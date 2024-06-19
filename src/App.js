7a24ea624b04d6781eeea4bea97971b584fd7adb
Lingaraj Kunda <lingarajkunda@gmail.com>
2024-06-20 01:19:41 +0530

    cicd check

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>My First Deployment</p>
+        <p>My First Deployment CICD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
