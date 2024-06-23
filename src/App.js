1435593777d945afdf01c3b52073f4695ec58882
lingaraj14 <49620618+lingaraj14@users.noreply.github.com>
2024-06-24 01:15:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React AAAAA
+          Learn React
         </a>
       </header>
     </div>
